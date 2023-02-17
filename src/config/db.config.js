db0d9f23e7b9329ea191d2021305b713c6c29c2c
UsmanFarooq46 <usman4609903@gmail.com>
2023-02-17 15:32:31 +0500

    db connect

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 function connectMongoos() {
   mongoose
-    .connect(process.env.DB_connect, { useNewUrlParser: true })
+    // .connect(process.env.DB_connect, { useNewUrlParser: true })
+    .connect(
+      "mongodb+srv://usman_farooq:usman_farooq@cluster0.zc8iddw.mongodb.net/?retryWrites=true&w=majority"
+    )
     .then(() => {
       console.log("Data Base connected");
     })
